d5ff38bc19f8c702e9c71b0129ad76e998ebee79
Szeng89 <shijinz898@gmail.com>
2024-03-06 08:02:14 -0500

    made the methods

--- src/main/java/com/github/zipcodewilmington/Game.java
+++ src/main/java/com/github/zipcodewilmington/Game.java
@@ -1,5 +1,6 @@
 package com.github.zipcodewilmington;
 
+import java.util.Scanner;
 
 /**
  * @author xt0fer
@@ -7,7 +8,39 @@ package com.github.zipcodewilmington;
  * @date 5/27/21 11:02 AM
  */
 public class Game {
+    Scanner
 
+    public void runGame() {}
 
+    //that prints a welcome message
+    public void announce_game(){
+        System.out.println("Welcome");
+    }
+    //print game over
+    public void game_over(){
+        System.out.println("game over");
+    }
+    //method char[] for secret word
+    public void initialize_game_state(){}
+    //return a char from player input
+    public void get_next_guess(){}
+    // return boolean
+    public void is_word_guessed(){}
+    // returns boolean
+    public void ask_to_play_again(){}
+    //Use printArray to show play where they are at
+    public void print_current_state(){}
+    //LOOP through the word array looking for the inputed guess, and replace the "_" with the guesses char if found
+    public void process(){}
+    //print a happy message
+    public void play_won(){
+        System.out.println("Yay! congrats you got the word!");
+    }
+    //print a sad message (){}
+    public void player_lost(){
+        System.out.println("Better luck next time");
+    }
+
+    }
 }
 
---
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -0,0 +1,12 @@
+package com.github.zipcodewilmington;
+
+public class Wordguess {
+
+
+    public static void main(String[] args) {
+        Game run = new Game();
+        run.runGame();
+    }
+
+
+}
