d350a9952030c8e3939fe3f45ca15233883f39d3
Szeng89 <shijinz898@gmail.com>
2024-03-06 14:25:03 -0500

    added random word to be chosen from array for secret word

--- src/main/java/com/github/zipcodewilmington/Game.java
+++ src/main/java/com/github/zipcodewilmington/Game.java
@@ -1,5 +1,6 @@
 package com.github.zipcodewilmington;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -8,12 +9,15 @@ import java.util.Scanner;
  * @date 5/27/21 11:02 AM
  */
 public class Game {
-    Scanner
 
-    public void runGame() {}
+    public static void runGame() {
+        announce_game();
+        getsecret_words();
+
+    }
 
     //that prints a welcome message
-    public void announce_game(){
+    public static void announce_game(){
         System.out.println("Welcome");
     }
     //print game over
@@ -21,26 +25,42 @@ public class Game {
         System.out.println("game over");
     }
     //method char[] for secret word
-    public void initialize_game_state(){}
+    public static void getsecret_words(){
+        //make array for random words
+        String[] words= {"cat", "dog", "bog", "cut"};
+
+        //Make random method object
+        Random rando = new Random();
+
+        //assigned a random number of the  []length to a int called randomNumber
+        int randomNumber = rando.nextInt(words.length);
+
+        // assign that random chosen []length to randomword
+        String randomWord = words[randomNumber];
+
+        //print to console randomWord
+        System.out.println(randomWord);
+
+    }
+
+    public static void initialize_game_state(){}
     //return a char from player input
-    public void get_next_guess(){}
+    public static void get_next_guess(){}
     // return boolean
-    public void is_word_guessed(){}
+    public static void is_word_guessed(){}
     // returns boolean
-    public void ask_to_play_again(){}
+    public static void ask_to_play_again(){}
     //Use printArray to show play where they are at
-    public void print_current_state(){}
+    public static void print_current_state(){}
     //LOOP through the word array looking for the inputed guess, and replace the "_" with the guesses char if found
-    public void process(){}
+    public static void process(){}
     //print a happy message
-    public void play_won(){
+    public static void play_won(){
         System.out.println("Yay! congrats you got the word!");
     }
     //print a sad message (){}
-    public void player_lost(){
+    public static void player_lost(){
         System.out.println("Better luck next time");
     }
 
-    }
 }
-
