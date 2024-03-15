8e5489fea552f272fa1d39086355e443803ebbc7
Szeng89 <shijinz898@gmail.com>
2024-03-14 22:04:24 -0400

    finally back to do this one that i skipped, added most of the methods

--- src/main/java/com/github/zipcodewilmington/Game.java
+++ src/main/java/com/github/zipcodewilmington/Game.java
@@ -10,57 +10,5 @@ import java.util.Scanner;
  */
 public class Game {
 
-    public static void runGame() {
-        announce_game();
-        getsecret_words();
-
-    }
-
-    //that prints a welcome message
-    public static void announce_game(){
-        System.out.println("Welcome");
-    }
-    //print game over
-    public void game_over(){
-        System.out.println("game over");
-    }
-    //method char[] for secret word
-    public static void getsecret_words(){
-        //make array for random words
-        String[] words= {"cat", "dog", "bog", "cut"};
-
-        //Make random method object
-        Random rando = new Random();
-
-        //assigned a random number of the  []length to a int called randomNumber
-        int randomNumber = rando.nextInt(words.length);
-
-        // assign that random chosen []length to randomword
-        String randomWord = words[randomNumber];
-
-        //print to console randomWord
-        System.out.println(randomWord);
-
-    }
-
-    public static void initialize_game_state(){}
-    //return a char from player input
-    public static void get_next_guess(){}
-    // return boolean
-    public static void is_word_guessed(){}
-    // returns boolean
-    public static void ask_to_play_again(){}
-    //Use printArray to show play where they are at
-    public static void print_current_state(){}
-    //LOOP through the word array looking for the inputed guess, and replace the "_" with the guesses char if found
-    public static void process(){}
-    //print a happy message
-    public static void play_won(){
-        System.out.println("Yay! congrats you got the word!");
-    }
-    //print a sad message (){}
-    public static void player_lost(){
-        System.out.println("Better luck next time");
-    }
 
 }
--- src/main/java/com/github/zipcodewilmington/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -1,12 +1,71 @@
 package com.github.zipcodewilmington;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 public class Wordguess {
-
+    Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Game run = new Game();
+        Wordguess run = new Wordguess();
         run.runGame();
     }
+
+    private void runGame() {
+
+    }
+
+    public String getRandomWord(List<String> words) {
+        Random random = new Random():
+    }
+
+    public char getNextGuess() {
+        System.out.println("Enter a letter: ");
+        return scanner.next().charAt(0);
+
+    }
+
+    public boolean askToPlayAgain() {
+        while (true) {
+            System.out.println("Play again? (yes/no)");
+            String userInput = scanner.next();
+
+            if (userInput.equals("yes")) {
+                return true;
+            }else if (userInput.equals("no")) {
+                return false;
+            }else {
+                System.out.println("Invalid input, Please enter yes or no");
+            }
+        }
+    }
+
+    public void playerWon() {
+        System.out.println("Wow! you did it! you win!!");
+
+    }
+
+    public void playerLost() {
+        System.out.println("Sorry, Maybe next time");
+
+    }
+
+    public List<String> getRandomWords(){
+        List<String> randomWords = new ArrayList<>();
+        randomWords.add("cat");
+        randomWords.add("dog");
+        randomWords.add("Bird");
+        randomWords.add("moon");
+        randomWords.add("apple");
+        randomWords.add("beach");
+        randomWords.add("banana");
+        randomWords.add("garden");
+        randomWords.add("library");
+        randomWords.add("monster");
+                
+
+    }
+
 
 
 }
