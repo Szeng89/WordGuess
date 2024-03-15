35dc54baef91521f78f7cc59c64445f22b7d7c08
Szeng89 <shijinz898@gmail.com>
2024-03-14 22:46:23 -0400

    FINISHED with help cohort help

--- src/main/java/com/github/zipcodewilmington/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -7,16 +7,52 @@ public class Wordguess {
     Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Wordguess run = new Wordguess();
+        Wordguess run = new Wordguess(); // Create instance of WordGuess game and start it
         run.runGame();
     }
 
     private void runGame() {
+        List<String> wordList = getRandomWords(10); // generate list of random words
+        System.out.println("Hi lets play WordGuess");
 
+        while (true) {
+            String secretWord = getRandomWord(wordList); // get random word from arraylist as secret word
+            StringBuilder sb = new StringBuilder("_".repeat(secretWord.length())); // uses _ to shows whats revealed
+            int numberOfGuessesLeft = secretWord.length(); // set number of guess left to length of word
+
+            while (numberOfGuessesLeft > 0) {
+                System.out.println("Currently Revealed: " +sb); // display whats revealed and guesses left
+                System.out.println("Number of guesses left: " + numberOfGuessesLeft);
+                char guess = getNextGuess();
+
+                if (secretWord.indexOf(guess) != -1) { //check if guessed charact is in secret word
+                    for (int i = 0; i < secretWord.length(); i++) {
+                        if (secretWord.charAt(i) == guess) { // if guess is right sets to shows in word
+                            sb.setCharAt(i, guess);
+                        }
+                    }
+                }
+                if (secretWord.equals(sb.toString())) { // check to see if entire word is guessed
+                    playerWon();
+                    break;
+                }
+
+                numberOfGuessesLeft--; // decrease guesses left
+            }
+            if (numberOfGuessesLeft == 0) { // if not more guesses player lost
+                playerLost();
+            }
+
+            if (!askToPlayAgain()) {
+                break;
+            }
+        }
     }
 
     public String getRandomWord(List<String> words) {
-        Random random = new Random():
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.size()); // get a random index from within the range of word list
+        return words.get(randomIndex);
     }
 
     public char getNextGuess() {
@@ -50,7 +86,7 @@ public class Wordguess {
 
     }
 
-    public List<String> getRandomWords(){
+    public List<String> getRandomWords(int word){ // not positive why i need a unused var in parameter
         List<String> randomWords = new ArrayList<>();
         randomWords.add("cat");
         randomWords.add("dog");
@@ -62,7 +98,7 @@ public class Wordguess {
         randomWords.add("garden");
         randomWords.add("library");
         randomWords.add("monster");
-                
+                return randomWords;
 
     }
 
